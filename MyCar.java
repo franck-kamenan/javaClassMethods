cb3e7c17c3944297d21585dec51d2a9654a01e0d
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-15 13:41:00 +0200

    Java Class Methods

---
+++ MyCar.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.javaClassMethods;
+
+public class MyCar
+{
+    public void acceleration()
+    {
+        System.out.println("La voiture accélère.");
+    }
+    public void vitesse(int vitesseMax)
+    {
+        System.out.println("La vitesse maximale de la voiture est: " + vitesseMax);
+    }
+}
---
+++ MyMain.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.javaClassMethods;
+
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+        MyCar maVoiture = new MyCar();
+        maVoiture.acceleration();
+        maVoiture.vitesse(360);
+    }
+
+
+}
